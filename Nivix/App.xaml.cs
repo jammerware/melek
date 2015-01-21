91256b38013cf0b7dcce9b0158a10b2f7dbc234d
Ben Stein <ben.s.stein@gmail.com>
2015-01-21 15:34:55 -0600

    Added Nivix, a management console for Melek.

---
+++ Nivix/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using FirstFloor.ModernUI.Presentation;
+
+namespace Nivix
+{
+    public partial class App : Application
+    {
+        private void this_Startup(object sender, StartupEventArgs e)
+        {
+            // set up MUI theme
+            Color myAccentColor = (Color)App.Current.FindResource("MyAccentColor");
+            AppearanceManager.Current.AccentColor = myAccentColor;
+        }
+    }
+}
---
+++ Nivix/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+
+namespace Nivix.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
