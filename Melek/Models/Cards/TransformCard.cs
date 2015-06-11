b8ab0f53673e92ee3a12e665b304ecffa7d5f81a
Ben Stein <ben.s.stein@gmail.com>
2015-06-11 16:43:43 -0500

    More refactoring.

---
+++ Melek/Models/Cards/TransformCard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Melek.Models
+{
+    public class TransformCard : CardBase<TransformPrinting>
+    {
+        // general
+        public CardCostCollection Cost { get; set; }
+
+        // front
+        public int? NormalPower { get; set; }
+        public string NormalText { get; set; }
+        public IReadOnlyList<CardType> NormalCardTypes { get; set; }
+        public int? NormalToughness { get; set; }
+        public IReadOnlyList<string> NormalTribes { get; set; }
+
+        // back
+        public CardType[] TransformedCardTypes { get; set; }
+        public int? TransformedPower { get; set; }
+        public string TransformedText { get; set; }
+        public int? TransformedToughness { get; set; }
+        public IReadOnlyList<string> TransformedTribes { get; set; }
+
+        #region enforced by CardBase<T>
+        public override IReadOnlyList<TransformPrinting> Printings { get; set; }
+        public override bool IsColor(MagicColor color)
+        {
+            return Cost.IsColor(color);
+        }
+        #endregion
+    }
+}
---
+++ Nivix/Models/ICardFactory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Bazam.Modules;
+using Melek.Models;
+
+namespace Nivix.Models
+{
+    public class ICardFactory
+    {
+        public List<Set> Sets { get; set; }
+
+        public ICard GetCard(XElement cardData)
+        {
+            // declare some generally useful things
+            ICard retVal = null;
+            string name = XMLPal.GetString(cardData.Element("name"));
+            string text = XMLPal.GetString(cardData.Element("ability"));
+
+            // flip cards (like Erayo, Soratami Ascendant) have ——— in their text
+            // split cards (like Beck // Call) have // in their name
+            // transforming cards (like Huntmaster of the Fells) have a back_id property in the source db
+
+            if(cardData.Element("back_id") != null) {
+                // TRANSFORMERS - MORE THAN MEETS THE EYE
+                TransformCard card = new TransformCard();
+                retVal = card as ICard;
+            }
+            else if (name.Contains(" // ")) {
+                // split card
+
+            }
+            else if (text.Contains("———")) {
+                // flip card
+            }
+            else {
+                // reg'lar card, y'all
+                Card card = new Card();
+                retVal = card as ICard;
+            }
+
+            return retVal;
+        }
+    }
+}
